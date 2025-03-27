7a673b909a5d5a45872453e331ab8e73466f283f
shelmith miano <mianoshelmith66@gmail.com>
2025-03-27 21:35:38 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
+const express = require('express');
+const app = express();
+const port = 3020;
 
-const express = require('express')// import express from "express";
+// Middleware to parse JSON
+app.use(express.json());
 
-const app = express()
-const port = 4000
+// Sample data
+let users = [
+    { id: 1, name: "Miano", age: 20 },
+    { id: 2, name: "Shelmith", age: 25 }
+];
 
+// GET all users
+app.get('/users', (req, res) => {
+    res.json(users);
+});
 
-//handles GET requests
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// GET a single user by ID
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    user ? res.json(user) : res.status(404).json({ message: "User not found" });
+});
 
-//start server
+// POST - Add a new user
+app.post('/users', (req, res) => {
+    const newUser = { id: users.length + 1, ...req.body };
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+// PUT - Update a user
+app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (user) {
+        Object.assign(user, req.body);
+        res.json(user);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+// DELETE - Remove a user
+app.delete('/users/:id', (req, res) => {
+    users = users.filter(u => u.id !== parseInt(req.params.id));
+    res.json({ message: "User deleted" });
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
-
+    console.log(`Server running at http://localhost:${port}`);
+});
